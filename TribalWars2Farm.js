65504af96aedc581285e2e098c83d98a211adf84
MrSloppy <timovanetten@hotmail.com>
2016-01-13 21:00:07 +0100

    Nu met functies!

--- TribalWars2Farm.js
+++ TribalWars2Farm.js
@@ -12,84 +12,83 @@ window.routeProvider = window.injector.get('routeProvider');
 var barbfarm = []
 var farmpresets = []
 
-window.setTimeout(3000)
-
 // Dit is de funcite om de farm presets op te slaan voor later gebruik
-socketService.emit(routeProvider.GET_PRESETS, {}, function(data){
-	for(i=0; i < data.presets.length; i++){
-		if ((data.presets[i].name).indexOf("farm") >= 0){
-			console.log(data.presets[i].name)
-			console.log(data.presets[i].id)
-			farmpresets.push(data.presets[i].id)
+function maakPresetLijst(){
+	socketService.emit(routeProvider.GET_PRESETS, {}, function(data){
+		for(i=0; i < data.presets.length; i++){
+			if ((data.presets[i].name).indexOf("farm") >= 0){
+				console.log(data.presets[i].name)
+				console.log(data.presets[i].id)
+				farmpresets.push(data.presets[i].id)
+			}
 		}
-	}
-});
-
-window.setTimeout(3000)
+	});
+	setTimeout(maakFarmLijst, 3000)
+}
 
 // Spear farm preset id: 1192659
 // Sword farm preset id: 1367804
 // Met deze krijg je alle info over omliggende barbaren dorpen en worden hun IDs opgeslagen in de array barbfarm
-socketService.emit(routeProvider.MAP_GETVILLAGES, {x:411, y:543, width:25, height:25}, function(data){
-	for (i=0; i < data.villages.length; i++){
-		if (data.villages[i].character_name == null){
-			console.log(data.villages[i])
-			barbfarm.push(data.villages[i].id)
+function maakFarmLijst(){
+	socketService.emit(routeProvider.MAP_GETVILLAGES, {x:411, y:543, width:25, height:25}, function(data){
+		for (i=0; i < data.villages.length; i++){
+			if (data.villages[i].character_name == null){
+				console.log(data.villages[i])
+				barbfarm.push(data.villages[i].id)
+			}
 		}
-	}
-});
+	});
+	setTimeout(stuurFarmLijst, 4000)
+}
 
-window.setTimeout(3000)
 
 //Dit wordt de loop die de farms vervolgens verstuurt.
-for (i=0; i < barbfarm.length; i++){
-  try{
-   socketService.emit(routeProvider.SEND_PRESET,  {start_village: 9993, target_village: barbfarm[i], army_preset_id: farmpresets[0], type: 'attack'}, function(data){
-   barbfarm.splice(i, 1);
-   })
-  }
-  catch(){
-		console.log("Geen troepen meer!!!")
-    break
-  }
-};
+function stuurFarmLijst(){
+	for (i=0; i < barbfarm.length; i++){
+	  try{
+	   socketService.emit(routeProvider.SEND_PRESET,  {start_village: 9993, target_village: barbfarm[i], army_preset_id: farmpresets[0], type: 'attack'}, function(data){
+	   barbfarm.splice(i, 1);
+	   })
+	  }
+	  catch(){
+			console.log("Geen troepen meer!!!")
+	    break
+	  }
+	};
 
-window.setTimeout(3000)
+	for (i=0; i < barbfarm.length; i++){
+	  try{
+	   socketService.emit(routeProvider.SEND_PRESET,  {start_village: 9993, target_village: barbfarm[i], army_preset_id: farmpresets[1], type: 'attack'}, function(data){
+	   barbfarm.splice(i, 1);
+	   })
+	  }
+	  catch(){
+			console.log("Geen troepen meer!!!")
+	    break
+	  }
+	};
 
-for (i=0; i < barbfarm.length; i++){
-  try{
-   socketService.emit(routeProvider.SEND_PRESET,  {start_village: 9993, target_village: barbfarm[i], army_preset_id: farmpresets[1], type: 'attack'}, function(data){
-   barbfarm.splice(i, 1);
-   })
-  }
-  catch(){
-		console.log("Geen troepen meer!!!")
-    break
-  }
-};
+	for (i=0; i < barbfarm.length; i++){
+	  try{
+	   socketService.emit(routeProvider.SEND_PRESET,  {start_village: 10805, target_village: barbfarm[i], army_preset_id: farmpresets[0], type: 'attack'}, function(data){
+	   barbfarm.splice(i, 1);
+	   })
+	  }
+	  catch(err){
+			console.log("Geen troepen meer!!!")
+	    break
+	  }
+	};
 
-for (i=0; i < barbfarm.length; i++){
-  try{
-   socketService.emit(routeProvider.SEND_PRESET,  {start_village: 10805, target_village: barbfarm[i], army_preset_id: farmpresets[0], type: 'attack'}, function(data){
-   barbfarm.splice(i, 1);
-   })
-  }
-  catch(err){
-		console.log("Geen troepen meer!!!")
-    break
-  }
-};
-
-window.setTimeout(3000)
-
-for (i=0; i < barbfarm.length; i++){
-  try{
-   socketService.emit(routeProvider.SEND_PRESET,  {start_village: 10805, target_village: barbfarm[i], army_preset_id: farmpresets[1], type: 'attack'}, function(data){
-   barbfarm.splice(i, 1);
-   })
-  }
-  catch(err){
-		console.log("Geen troepen meer!!!")
-    break
-  }
-};
+	for (i=0; i < barbfarm.length; i++){
+	  try{
+	   socketService.emit(routeProvider.SEND_PRESET,  {start_village: 10805, target_village: barbfarm[i], army_preset_id: farmpresets[1], type: 'attack'}, function(data){
+	   barbfarm.splice(i, 1);
+	   })
+	  }
+	  catch(err){
+			console.log("Geen troepen meer!!!")
+	    break
+	  }
+	};
+}
